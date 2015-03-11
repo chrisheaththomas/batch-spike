99a439a964f6b14e32a9a34714c84d30a6e2188d
chris <chrisheaththomas@hotmail.com>
2015-03-11 17:37:21 +0000

    Add links

--- script/provision.sh
+++ script/provision.sh
@@ -4,4 +4,10 @@ apt-get update
 apt-get install -y alien
 alien -i /home/vagrant/oracle-instantclient11.1-basic-11.1.0.7.0-1.x86_64.rpm
 alien -i /home/vagrant/cx_Oracle-5.1.2-11g-py27-1.x86_64.rpm
-#TODO add links + install libaio1 and libpython2.7
+apt-get install -y libpython2.7
+apt-get install -y libaio1
+ln -s /usr/lib/python2.7/site-packages/cx_Oracle.so /usr/lib/python2.7/lib-dynload
+ln -s /usr/lib/oracle/11.1/client64/lib/libnnz11.so /usr/lib
+ln -s /usr/lib/oracle/11.1/client64/lib/libclntsh.so.11.1 /usr/lib
+#echo export LD_LIBRARY_PATH=/usr/lib/oracle/11.1/client/lib >> ~/.bashrc
+#source ~/.bashrc
